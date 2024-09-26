91f420deffa03e1b95f82952edd09592786c7a9d
Hasan Turken <turkenh@gmail.com>
2024-09-26 10:18:01 +0300

    Generate with RegistryConfig API
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- apis/pkg/v1alpha1/registry_config.go
+++ apis/pkg/v1alpha1/registry_config.go
@@ -1,13 +1,35 @@
 package v1alpha1
 
 import (
-	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 )
 
 // +kubebuilder:object:root=true
 // +genclient
 // +genclient:nonNamespaced
+
+// A RegistryConfig configures a registry for use by the package manager.
+// +kubebuilder:printcolumn:name="PREFIX",type="string",JSONPath=".spec.match.prefix"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:resource:scope=Cluster
+type RegistryConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec RegistryConfigSpec `json:"spec,omitempty"`
+	// TODO: Add status fields
+}
+
+// +kubebuilder:object:root=true
+
+// RegistryConfigList contains a list of RegistryConfig.
+type RegistryConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RegistryConfig `json:"items"`
+}
 
 // RegistryConfigMatch is a matcher that determines if a registry should be used.
 type RegistryConfigMatch struct {
@@ -40,6 +62,7 @@ type ConfigMapKeySelector struct {
 	Key string `json:"key" protobuf:"bytes,2,opt,name=key"`
 }
 
+// RegistryConfigSigning contains the configuration for image signing.
 type RegistryConfigSigning struct {
 	// Required indicates whether the signature is required for images from this
 	// registry.
@@ -49,7 +72,7 @@ type RegistryConfigSigning struct {
 	PublicKeyConfigMapRef ConfigMapKeySelector `json:"publicKeyConfigMapRef"`
 }
 
-// RegistryConfigSpec defines the desired state of RegistryConfig
+// RegistryConfigSpec defines the desired state of RegistryConfig.
 type RegistryConfigSpec struct {
 	// Match is a list of matchers that determine if this registry should be used.
 	Match RegistryConfigMatch `json:"match"`
@@ -59,25 +82,4 @@ type RegistryConfigSpec struct {
 	// Signing contains the configuration for image signing.
 	// +optional
 	Signing *RegistryConfigSigning `json:"signing,omitempty"`
-}
-
-// A RegistryConfig configures a registry for use by the package manager.
-// +kubebuilder:printcolumn:name="PREFIX",type="string",JSONPath=".spec.match.prefix"
-// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
-// +kubebuilder:resource:scope=Cluster
-type RegistryConfig struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec RegistryConfigSpec `json:"spec,omitempty"`
-	// TODO: Add status fields
-}
-
-// +kubebuilder:object:root=true
-
-// RegistryConfigList contains a list of RegistryConfig.
-type RegistryConfigList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []RegistryConfig `json:"items"`
 }
